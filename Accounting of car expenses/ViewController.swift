572d4c09f089bcf77d62f083060eae6cd5d1552b
DmytroHorodyskyi <107617154+DmytroHorodyskyi@users.noreply.github.com>
2023-06-09 07:59:10 +0300

    Initial Commit

---
+++ Accounting of car expenses/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Accounting of car expenses
+//
+//  Created by Dmytro Horodyskyi on 09.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
